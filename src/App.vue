fb38c3eafb6cf473be4725b5bc3e74afce51c125
ismailnarin <ismailnrn94@gmail.com>
2023-07-10 00:29:01 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <CalculatorComponent />
+</template>
+<script>
+import CalculatorComponent from "@/components/CalculatorComponent";
+export default {
+  components: {
+    CalculatorComponent: CalculatorComponent,
+  },
+};
+</script>
---
+++ src/components/CalculatorComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="calculatorContainer">
+    <div class="calculatorNumbers">
+      <div class="calculatorNumberItem">1</div>
+      <div class="calculatorNumberItem">2</div>
+      <div class="calculatorNumberItem">3</div>
+      <div class="calculatorNumberItem">4</div>
+      <div class="calculatorNumberItem">5</div>
+      <div class="calculatorNumberItem">6</div>
+      <div class="calculatorNumberItem">7</div>
+      <div class="calculatorNumberItem">8</div>
+      <div class="calculatorNumberItem">9</div>
+      <div class="calculatorNumberItem">0</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
