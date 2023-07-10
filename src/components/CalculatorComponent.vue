90c94386dba0af61f4d04a65b8cda0fbb3ebd70c
ismailnarin <ismailnrn94@gmail.com>
2023-07-10 18:03:01 +0300

    update

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -2,16 +2,10 @@
   <div ref="calculatorScreen" class="calculatorScreen"></div>
   <div class="calculatorContainer">
     <div class="calculatorNumbers">
-      <div class="calculatorNumberItem">AC</div>
-      <div class="calculatorNumberItem">%</div>
       <div v-for="number in calculateNumbers" :key="number" @click="addNumber" class="calculatorNumberItem">{{ number }}</div>
     </div>
     <div class="calculatorOperators">
-      <div class="calculatorOperatorItems">+</div>
-      <div class="calculatorOperatorItems">-</div>
-      <div class="calculatorOperatorItems">/</div>
-      <div class="calculatorOperatorItems">x</div>
-      <div class="calculatorOperatorItems">=</div>
+      <div v-for="operator in operators" :key="operator" @click="addOperator" class="calculatorOperatorItems">{{ operator }}</div>
     </div>
   </div>
 </template>
@@ -20,20 +14,24 @@ export default {
   data() {
   return {
     screenNumber:"",
-    calculateNumbers: [1,2,3,4,5,6,7,8,9,0,","]
+    calculateNumbers: ["AC","%",1,2,3,4,5,6,7,8,9,0,","],
+    operators:["+","-","/","x","="]
   }
 },
 methods: {
   addNumber(event){
     var buttonText = event.target.textContent;
-    this.screenNumber += buttonText.toString();
+    (buttonText=="AC" || buttonText=="%") ? buttonText="":this.screenNumber += buttonText.toString();
   },
+  addOperator(event){
+    console.log(event);
+  }
 },
 watch:{
   screenNumber(){
     const calculatorScreen=this.$refs.calculatorScreen;
     calculatorScreen.textContent=this.screenNumber;
-  }
+  },
 }
 };
 </script>
