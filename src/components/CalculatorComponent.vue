cbe6bb4089263110df69e41e422427f721bd0998
ismailnarin <ismailnrn94@gmail.com>
2023-07-10 14:16:54 +0300

    update

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -1,19 +1,39 @@
 <template>
+  <div ref="calculatorScreen" class="calculatorScreen"></div>
   <div class="calculatorContainer">
     <div class="calculatorNumbers">
-      <div class="calculatorNumberItem">1</div>
-      <div class="calculatorNumberItem">2</div>
-      <div class="calculatorNumberItem">3</div>
-      <div class="calculatorNumberItem">4</div>
-      <div class="calculatorNumberItem">5</div>
-      <div class="calculatorNumberItem">6</div>
-      <div class="calculatorNumberItem">7</div>
-      <div class="calculatorNumberItem">8</div>
-      <div class="calculatorNumberItem">9</div>
-      <div class="calculatorNumberItem">0</div>
+      <div class="calculatorNumberItem">AC</div>
+      <div class="calculatorNumberItem">%</div>
+      <div v-for="number in calculateNumbers" :key="number" @click="addNumber" class="calculatorNumberItem">{{ number }}</div>
+    </div>
+    <div class="calculatorOperators">
+      <div class="calculatorOperatorItems">+</div>
+      <div class="calculatorOperatorItems">-</div>
+      <div class="calculatorOperatorItems">/</div>
+      <div class="calculatorOperatorItems">x</div>
+      <div class="calculatorOperatorItems">=</div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+  return {
+    screenNumber:"",
+    calculateNumbers: [1,2,3,4,5,6,7,8,9,0,","]
+  }
+},
+methods: {
+  addNumber(event){
+    var buttonText = event.target.textContent;
+    this.screenNumber += buttonText.toString();
+  },
+},
+watch:{
+  screenNumber(){
+    const calculatorScreen=this.$refs.calculatorScreen;
+    calculatorScreen.textContent=this.screenNumber;
+  }
+}
+};
 </script>
