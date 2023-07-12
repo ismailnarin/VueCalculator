674d3f3965c2c1e8551b0424590996c01b57f51d
ismailnarin <ismailnrn94@gmail.com>
2023-07-12 15:41:55 +0300

    provide

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -1,156 +1,177 @@
 <template>
-  <div class="calculatorContainer">
-    <div class="calculatorNumbers">
-      <div @click="addOperator" key="AC" class="calculatorNumberItem">AC</div>
-      <div @click="addOperator" key="%" class="calculatorNumberItem">%</div>
-      <div v-for="number in calculateNumbers" :key="number" @click="addNumber" class="calculatorNumberItem">{{ number }}</div>
-    </div>
-    <div class="calculatorOperators">
-      <div v-for="operator in operators" :key="operator" @click="addOperator" class="calculatorOperatorItems">{{ operator }}</div>
-    </div>
-  </div>
+	<div class="calculatorContainer">
+		<div class="calculatorNumbers">
+			<div @click="addOperator" key="AC" class="calculatorNumberItem">AC</div>
+			<div @click="addOperator" key="%" class="calculatorNumberItem">%</div>
+			<div
+				v-for="number in calculateNumbers"
+				:key="number"
+				@click="addNumber"
+				class="calculatorNumberItem">
+				{{ number }}
+			</div>
+		</div>
+		<div class="calculatorOperators">
+			<div
+				v-for="operator in operators"
+				:key="operator"
+				@click="addOperator"
+				class="calculatorOperatorItems">
+				{{ operator }}
+			</div>
+		</div>
+	</div>
 </template>
 <style>
-.calculatorScreen {
-	font-family: monospace;
-	font-size: 100px;
-	display: flex;
-	width: 100%;
-	height: 300px;
-	color: white;
-}
-.calculatorContainer {
-	display: flex;
-	width: 60%;
-}
-.calculatorNumbers {
-	grid-template-columns: repeat(3, 100px);
-	display: grid;
-	gap: 10px;
-	grid-template-rows: repeat(5, 100px);
-}
-.calculatorNumberItem {
-	user-select: none;
-	cursor:pointer;
-	font-weight: 600;
-	color: white;
-	border-radius: 50px;
-	display: flex;
-	background-color: #2f2f2f;
-	justify-content: center;
-	align-items: center;
-	font-size: 25px;
-}
-.calculatorNumberItem:hover{
-	background-color: #3b3b3b;
-}
-.calculatorNumberItem:nth-last-child(2){
-	grid-column-start: 1;
-  grid-column-end: 3;
-}
-.calculatorNumberItem:nth-child(1){
-	grid-column-start: 1;
-  grid-column-end: 3;
-}
-
-.calculatorNumberItem:nth-child(1),
-	.calculatorNumberItem:nth-child(2){
-		background-color:#838383;
+	.calculatorContainer {
+		display: flex;
+		width: 60%;
+	}
+	.calculatorNumbers {
+		grid-template-columns: repeat(3, 100px);
+		display: grid;
+		gap: 10px;
+		grid-template-rows: repeat(5, 100px);
+	}
+	.calculatorNumberItem {
+		user-select: none;
+		cursor: pointer;
+		font-weight: 600;
+		color: white;
+		border-radius: 50px;
+		display: flex;
+		background-color: #2f2f2f;
+		justify-content: center;
+		align-items: center;
+		font-size: 25px;
+	}
+	.calculatorNumberItem:hover {
+		background-color: #3b3b3b;
+	}
+	.calculatorNumberItem:nth-last-child(2) {
+		grid-column-start: 1;
+		grid-column-end: 3;
+	}
+	.calculatorNumberItem:nth-child(1) {
+		grid-column-start: 1;
+		grid-column-end: 3;
 	}
 
-.calculatorOperators {
-    margin-left: 10px;
-    color: white;
-    grid-template-columns: repeat(1, 100px);
-    grid-template-rows: repeat(1, 100px);
-    display: grid;
-    gap: 10px;
-}
-.calculatorOperatorItems {
-	display: flex;
-	background-color: orange;
-	justify-content: center;
-	align-items: center;
-	border-radius: 50px;
-	font-size: 30px;
-	font-weight: bold;
-	cursor:pointer;
-	user-select: none;
-}
+	.calculatorNumberItem:nth-child(1),
+	.calculatorNumberItem:nth-child(2) {
+		background-color: #838383;
+	}
+
+	.calculatorOperators {
+		margin-left: 10px;
+		color: white;
+		grid-template-columns: repeat(1, 100px);
+		grid-template-rows: repeat(1, 100px);
+		display: grid;
+		gap: 10px;
+	}
+	.calculatorOperatorItems {
+		display: flex;
+		background-color: orange;
+		justify-content: center;
+		align-items: center;
+		border-radius: 50px;
+		font-size: 30px;
+		font-weight: bold;
+		cursor: pointer;
+		user-select: none;
+	}
 </style>
 <script>
-export default {
-  data() {
-  return {
-    calculateNumbers: [1,2,3,4,5,6,7,8,9,0,","],
-    operators:["+","-","/","x","="],
-    firstNumber:null,
-    secondNumber:null,
-    buttonOperator:null,
-    result:null,
-    operatorCounter:0,
-  }
-},
-methods: {
-  addNumber(event){
-    if(this.operatorCounter>0 && this.firstNumber!==null){
-      this.operatorCounter=0;
-      this.provideData.screenResult=""
-    }
-    var buttonText = event.target.textContent;
-    this.provideData.screenResult += buttonText.toString();
-  },
-  addOperator(event){
-    this.operatorCounter++;
-    if(event.target.textContent=="AC"){
-      this.buttonOperator=null;
-      this.result=null;
-      this.provideData.screenResult="";
-      this.firstNumber=null;
-      this.secondNumber=null;
-    }
-    else{
-      if(this.firstNumber==null){
-        var buttonText=event.target.textContent;
-        this.buttonOperator = buttonText;
-        this.firstNumber=this.provideData.screenResult;
-      }
-      else if(this.firstNumber!==null && this.secondNumber ==null && this.operatorCounter==1){
-        this.secondNumber = this.provideData.screenResult;
-        this.mathProcess(this.firstNumber, this.secondNumber, this.buttonOperator);
-      }
-      this.buttonOperator=event.target.textContent;
-    }
-  },
+	export default {
+		data() {
+			return {
+				calculateNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."],
+				operators: ["+", "-", "/", "x", "="],
+				firstNumber: null,
+				secondNumber: null,
+				buttonOperator: null,
+				result: null,
+				operatorCounter: 0,
+			};
+		},
+		methods: {
+			addNumber(event) {
+				if (this.operatorCounter > 0 && this.firstNumber !== null) {
+					this.operatorCounter = 0;
+					this.provideData.screenResult = "";
+				}
+				var buttonText = event.target.textContent;
+				this.provideData.screenResult += buttonText.toString();
+			},
+			addOperator(event) {
+				this.operatorCounter++;
+				if (event.target.textContent == "AC") {
+					this.buttonOperator = null;
+					this.result = null;
+					this.provideData.screenResult = "";
+					this.firstNumber = null;
+					this.secondNumber = null;
+				} else {
+					if (this.firstNumber == null) {
+						var buttonText = event.target.textContent;
+						this.buttonOperator = buttonText;
+						this.firstNumber = this.provideData.screenResult;
+					} else if (
+						this.firstNumber !== null &&
+						this.secondNumber == null &&
+						this.operatorCounter == 1
+					) {
+						this.secondNumber = this.provideData.screenResult;
+						this.mathProcess(
+							this.firstNumber,
+							this.secondNumber,
+							this.buttonOperator
+						);
+					}
+					this.buttonOperator = event.target.textContent;
+					this.provideData.screenResult = "";
+				}
+			},
 
-  mathProcess(firstNumber,secondNumber,operator){
-    switch (operator) {
-      case "+":
-        this.result = parseFloat(firstNumber)+parseFloat(secondNumber);
-        break;
-      case "-":
-        this.result = parseFloat(firstNumber)-parseFloat(secondNumber);
-        break;
-      case "x":
-        this.result = parseFloat(firstNumber)*parseFloat(secondNumber);
-        break;
-      case "/":
-        this.result = parseFloat(firstNumber)/parseFloat(secondNumber);
-        break;
-      case "%":
-        this.result = parseFloat(firstNumber)-(parseFloat(firstNumber)*(parseFloat(secondNumber)/100));
-        break;
-      case "=":
-        this.buttonOperator=null;
-        this.result=this.provideData.screenResult;
-        break;
-    }
-    this.secondNumber=null;
-    this.firstNumber=this.result;
-    this.provideData.screenResult=this.result;
-    this.result=null;
-  }
-},
-inject : ['provideData']
-};
+			mathProcess(firstNumber, secondNumber, operator) {
+				switch (operator) {
+					case "+":
+						this.result = parseFloat(firstNumber) + parseFloat(secondNumber);
+						break;
+					case "-":
+						this.result = parseFloat(firstNumber) - parseFloat(secondNumber);
+						break;
+					case "x":
+						this.result = parseFloat(firstNumber) * parseFloat(secondNumber);
+						break;
+					case "/":
+						this.result = parseFloat(firstNumber) / parseFloat(secondNumber);
+						break;
+					case "%":
+						this.result =
+							parseFloat(firstNumber) -
+							parseFloat(firstNumber) * (parseFloat(secondNumber) / 100);
+						break;
+					case "=":
+						this.buttonOperator = null;
+						this.result = this.provideData.screenResult;
+						break;
+				}
+				this.secondNumber = null;
+				this.firstNumber = this.result;
+				this.provideData.screenResult = this.result;
+				this.result = null;
+			},
+		},
+		inject: ["provideData"],
+		watch: {
+			firstNumber() {
+				this.provideData.screenFirstNumber = this.firstNumber;
+			},
+			buttonOperator() {
+				this.provideData.screenOperator = this.buttonOperator;
+			},
+		},
+	};
 </script>
